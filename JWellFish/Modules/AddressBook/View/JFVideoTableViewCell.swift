fc1c5df7940f485ed0eabcd51fb255b99b0991f2
ryanranya <ryanranya@163.com>
2020-04-02 02:34:41 +0800

    播放，暂停按钮

--- JWellFish/Modules/AddressBook/View/JFVideoTableViewCell.swift
+++ JWellFish/Modules/AddressBook/View/JFVideoTableViewCell.swift
@@ -11,10 +11,15 @@ import Reusable
 import SwiftyFitsize
 class JFVideoTableViewCell: UITableViewCell,NibReusable {
 
+    @IBOutlet weak var playOrPause: UIButton!
+    @IBOutlet weak var playImage: UIImageView!
     @IBOutlet weak var giveLikeButton: UIButton!
     @IBOutlet weak var collectionButton: UIButton!
     @IBOutlet weak var videoBgView: UIView!
     @IBOutlet weak var countLabel: UILabel!
+    
+    let isPlay: Bool? = nil
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,8 +27,17 @@ class JFVideoTableViewCell: UITableViewCell,NibReusable {
 //        videoView.frame = CGRect(x: 0, y: 0, width: KScreenWidth - 40~, height: 140~)
 //        videoBgView.addSubview(videoView)
         countLabel.text = "测试一下自适应的高度行不行卡机你那是氪金大佬抠脚大汉案件肯德基卡上"
+        self.playOrPause.isSelected = true
     }
-
+    
+    @IBAction func playOrPauseTap(_ sender: Any) {
+        self.playOrPause.isSelected = !self.playOrPause.isSelected
+        if self.playOrPause.isSelected {
+            self.playImage.image = UIImage.init(named: "play")
+        }else{
+            self.playImage.image = UIImage.init(named: "pause")
+        }
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
